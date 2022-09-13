4be0b0dac01178984f03408cd13e25c3e025452a
Gajendra <gajendrabg123@gmail.com>
2022-09-13 22:09:06 +0530

    Create practice_comments.py

---
+++ Practice/practice_comments.py
@@ -0,0 +1,14 @@
+
+"""
+Commenting
+# in a piece of code if we need remember a step we use comment before the code and
+ if we need to skip a line in code we use comment.
+
+we have two types of commenting
+1.singleline commenting : in this we add shift+3 before the line to add a comment
+                          we use ctrl+? : if we need to add a comment in that line
+                          if we need to add comment to a line after the code we use 'space Shift+3'
+2.multiline commenting: in this commenting we write comment in between three double quote open and
+                         three double quote closed
+"""
+print("comments")        #this is a comments example
