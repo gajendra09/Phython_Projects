521fde9720e98d9d7baa15eaf8bbb1e212167b6a
Gajendra <gajendrabg123@gmail.com>
2022-09-13 20:54:38 +0530

    comments updated and print_statements added

---
+++ Class_Practice/Print_Statements.py
@@ -0,0 +1,14 @@
+# first type of print statements
+
+print(8)
+
+# another way of print statements
+
+a = 10
+print("the value of a is ", a)
+print("The value of a using format method is {}".format(a))
+
+
+b = 1
+c = 2
+print("The value of a is {}, the value of b is {} and the vale of c is {}".format(a, b, c))
---
+++ Class_Practice/comments.py
@@ -0,0 +1,25 @@
+# single line commenting can be done using shift+3, This is dummy code of line
+# Anothervway of commenting line is ctrl+?  and the same way it can be uncommented
+
+
+# commenting can be done next to the code by having space after the code with shift+3
+
+print("Hello")              # It is printing Hello
+
+"""
+Multiple line commenting
+can be done between 3 double quotes opened 
+and 3 diuble quotes closed
+"""# single line commenting can be done using shift+3, This is dummy code of line
+# Anothervway of commenting line is ctrl+?  and the same way it can be uncommented
+
+
+# commenting can be done next to the code by having space after the code with shift+3
+
+print("Hello")              # It is printing Hello
+
+"""
+Multiple line commenting
+can be done between 3 double quotes opened 
+and 3 diuble quotes closed
+"""
