791f731b0b04cbd16ea6fe554ab08f41b456e059
Gajendra <gajendrabg123@gmail.com>
2022-09-13 22:35:13 +0530

    Create practice_print.py

---
+++ Practice/practice_print.py
@@ -0,0 +1,27 @@
+"""
+Print: we use this to display the output
+
+we use different ways to print the output
+1.we print the out put without adding any integer
+  ex:print('3')
+2.we assing a value to the integer and we print the integer
+  ex: a=2
+      print("the value of a is ", a)
+3.if we want to print multiple values in a single line we use
+  1.we use {}
+      ex: the value of a is {}
+  2.we use .format and add integer to .format
+       ex: print(" the value of a is {}, the value of b is {}, the value of c is {} ".format(a, b, c))
+"""
+
+print("2")
+
+x = 5
+print("the value of x is", x)
+
+m=3
+n=5
+o=87
+p=887
+
+print("the value of x is {}, the value of m is {}, the value of n is {}, the value of o is {}, the value of p is {}".format(x, m, n, o, p))
