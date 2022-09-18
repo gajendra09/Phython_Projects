659c7146fc05b582bbe19351a40aaf11d456ec80
Gajendra <gajendrabg123@gmail.com>
2022-09-18 11:59:05 +0530

    Update practice_string.py

--- Practice/practice_string.py
+++ Practice/practice_string.py
@@ -1,5 +1,86 @@
-str_x = "Hello Welcome"
-str_y = "Python@3.7"
-str_z = "DataType"
+
+
 
 # Find out the length of strings given and find the indexing of given strings
+
+str_x = "Hello Welcome"
+len_str_x = len(str_x)
+print("the length of string x is {}".format(len_str_x))
+
+zer_ind_str_x = str_x[0]
+print("the value of zeroth index of string x is {}".format(zer_ind_str_x))
+frst_ind_str_x = str_x[1]
+print("the value of fisrt index of string x is {}".format(frst_ind_str_x))
+scnd_ind_str_x = str_x[2]
+print("the value of second index of string x is {}".format(scnd_ind_str_x))
+thrd_ind_str_x = str_x[3]
+print("the value of third index of sting x is {}".format(thrd_ind_str_x))
+frth_ind_str_x = str_x[4]
+print("the value of fourth index of string is {}".format(frth_ind_str_x))
+fth_ind_str_x = str_x[5]
+print("the value of fifth index of string x is {}".format(fth_ind_str_x))
+sxth_ind_str_x = str_x[6]
+print("the value of sixth index of string x is {}".format(sxth_ind_str_x))
+sevnth_ind_str_x = str_x[7]
+print("the value of seventh index of string x is {}".format(sevnth_ind_str_x))
+egth_ind_str_x = str_x[8]
+print("the value of eighth index of string x is {}".format(egth_ind_str_x))
+ninth_ind_str_x = str_x[9]
+print("the value of ninth index of string x is {}".format(ninth_ind_str_x))
+tnth_ind_str_x = str_x[10]
+print("the value of tenth index of string x is {}".format(tnth_ind_str_x))
+elvnth_ind_str_x = str_x[11]
+print("the value of eleventh index of string x is {}".format(elvnth_ind_str_x))
+twlth_ind_str_x = str_x[12]
+print("the value of twelveth index of string x is {}".format(twlth_ind_str_x))
+
+str_y = "Python@3.7"
+len_str_y = len(str_y)
+print("the lenth of string y is {}".format(len_str_y))
+
+zer_ind_str_y = str_y[0]
+print("the value of zeroth index of string y is {}".format(zer_ind_str_y))
+frst_ind_str_y = str_y[1]
+print("the value of fisrt index of string y is {}".format(frst_ind_str_y))
+scnd_ind_str_y = str_y[2]
+print("the value of second index of string y is {}".format(scnd_ind_str_y))
+thrd_ind_str_y = str_y[3]
+print("the value of third index of sting y is {}".format(thrd_ind_str_y))
+frth_ind_str_y = str_y[4]
+print("the value of fourth index of string y is {}".format(frth_ind_str_y))
+fth_ind_str_y = str_y[5]
+print("the value of fifth index of string y is {}".format(fth_ind_str_y))
+sxth_ind_str_y = str_y[6]
+print("the value of sixth index of string y is {}".format(sxth_ind_str_y))
+sevnth_ind_str_y = str_y[7]
+print("the value of seventh index of string y is {}".format(sevnth_ind_str_y))
+egth_ind_str_y = str_y[8]
+print("the value of eighth index of string y is {}".format(egth_ind_str_y))
+ninth_ind_str_y = str_y[9]
+print("the value of ninth index of string y is {}".format(ninth_ind_str_y))
+
+str_z = "DataType"
+len_str_z = len(str_z)
+print("the length of sting z is {}".format(len_str_z))
+
+zer_ind_str_z = str_z[0]
+print("the value of zeroth index of string z is {}".format(zer_ind_str_z))
+frst_ind_str_z = str_z[1]
+print("the value of fisrt index of string z is {}".format(frst_ind_str_z))
+scnd_ind_str_z = str_z[2]
+print("the value of second index of string z is {}".format(scnd_ind_str_z))
+thrd_ind_str_z = str_z[3]
+print("the value of third index of sting z is {}".format(thrd_ind_str_z))
+frth_ind_str_z = str_z[4]
+print("the value of fourth index of string z is {}".format(frth_ind_str_z))
+fth_ind_str_z = str_z[5]
+print("the value of fifth index of string z is {}".format(fth_ind_str_z))
+sxth_ind_str_z = str_z[6]
+print("the value of sixth index of string z is {}".format(sxth_ind_str_z))
+sevnth_ind_str_z = str_z[7]
+print("the value of seventh index of string z is {}".format(sevnth_ind_str_z))
+
+
+
+
+
