4832996ac7fc991d4539dc935134c11c39f14747
Gajendra <gajendrabg123@gmail.com>
2022-09-19 22:53:24 +0530

    negative indexing

--- Class_Practice/Data_Type_String.py
+++ Class_Practice/Data_Type_String.py
@@ -56,3 +56,36 @@ frth_ind_str = str[4]
 print("The value of fourth index is {}".format(frth_ind_str))
 fft_ind_str = str[5]
 print("The value of fifth index is {}".format(fft_ind_str))
+
+# Reverse indexing
+
+str = "python"
+
+frst_neg_str = str[-1]
+print("The value of first negative index of string is {}".format(frst_neg_str))
+
+scnd_neg_str = str[-2]
+print("The value of second negative index of string is {}".format(scnd_neg_str))
+
+thrd_neg_str = str[-3]
+print("The value of third negative index of string is {}".format(thrd_neg_str))
+
+frth_neg_str = str[-4]
+print("The value of fourth negative index of string is {}".format(frth_neg_str))
+
+fif_neg_str = str[-5]
+print("The value of fifth negative index of string is {}".format(fif_neg_str))
+
+six_neg_str = str[-6]
+print("The value of sixth negative index of string is {}".format(six_neg_str))
+
+# Slicing
+
+str_zer_to_sec_ind = str[0:3]
+print("The value of slicing from zero to second index of a string is {}".format(str_zer_to_sec_ind))
+
+str_two_to_fifth = str[2:6]
+print("The value of slicing from second to fifth index of a string is {}".format(str_two_to_fifth))
+
+str_four_to_five = str[4:6]
+print("The value of slicing from fourth to fifth index of string is {}".format(str_four_to_five))
--- Practice/practice_string.py
+++ Practice/practice_string.py
@@ -34,6 +34,34 @@ print("the value of eleventh index of string x is {}".format(elvnth_ind_str_x))
 twlth_ind_str_x = str_x[12]
 print("the value of twelveth index of string x is {}".format(twlth_ind_str_x))
 
+frst_neg_str_x = str_x[-1]
+print("the value of first negative index of string x is {}".format(frst_neg_str_x))
+scnd_neg_str_x = str_x[-2]
+print("the value of second negative index of string x is {}".format(scnd_neg_str_x))
+thrd_neg_str_x = str_x[-3]
+print("the value of third negative index of string x is {}".format(thrd_neg_str_x))
+frth_neg_str_x = str_x[-4]
+print("the value of fourth negative index of string x is {}".format(frth_neg_str_x))
+fth_neg_str_x = str_x[-5]
+print("the value of fifth negative index of string x is {}".format(fth_neg_str_x))
+sxth_neg_str_x = str_x[-6]
+print("the value of sixth negative index of string x is {}".format(sxth_neg_str_x))
+snth_neg_str_x = str_x[-7]
+print("the value of seventh negative index of  string x is {}".format(snth_neg_str_x))
+egth_neg_str_x = str_x[-8]
+print("the value of eighth negative index of string x is {}".format(egth_neg_str_x))
+ninth_neg_str_x = str_x[-9]
+print("the value of ninth negative index of string x is {}".format(ninth_neg_str_x))
+tnth_neg_str_x = str_x[-10]
+print("the value of tenth negative index of string x is {}".format(tnth_neg_str_x))
+elnth_neg_str_x = str_x[-11]
+print("the value of eleventh negative index of string x is {}".format(elnth_neg_str_x))
+twlth_neg_str_x = str_x[-12]
+print("the value of twelveth negative index of string x is {}".format(twlth_neg_str_x))
+trtnth_neg_str_x = str_x[-13]
+print("the value of thirteenth negative index of string x is {}".format(trtnth_neg_str_x))
+
+
 str_y = "Python@3.7"
 len_str_y = len(str_y)
 print("the lenth of string y is {}".format(len_str_y))
@@ -59,6 +87,29 @@ print("the value of eighth index of string y is {}".format(egth_ind_str_y))
 ninth_ind_str_y = str_y[9]
 print("the value of ninth index of string y is {}".format(ninth_ind_str_y))
 
+frst_neg_str_y = str_y[-1]
+print("the value of first negative index of string y is {}".format(frst_neg_str_y))
+scnd_neg_str_y = str_y[-2]
+print("the value of second negative index of string y is {}".format(scnd_neg_str_y))
+thrd_neg_str_y = str_y[-3]
+print("the value of third negative index of y is {}".format(thrd_neg_str_y))
+frth_neg_str_y = str_y[-4]
+print("the value of fourth negative index of string y is {}".format(frth_neg_str_y))
+ffth_neg_str_y = str_y[-5]
+print("the value of fifth negative index of string y is {}".format(ffth_neg_str_y))
+sxth_neg_str_y = str_y[-6]
+print("the value of sixth negative index of string y is {}".format(sxth_neg_str_y))
+snth_neg_str_y = str_y[-7]
+print("the value of seventh negative index of string y is {}".format(snth_neg_str_y))
+egth_neg_str_y = str_y[-8]
+print("the value of eighth negative index of string y is {}".format(egth_neg_str_y))
+ninth_neg_str_y = str_y[-9]
+print("the value of ninth negative index of string y is {}".format(ninth_neg_str_y))
+tnth_neg_str_y = str_y[-10]
+print("the value of tenth negative index of string y is {}".format(tnth_neg_str_y))
+
+
+
 str_z = "DataType"
 len_str_z = len(str_z)
 print("the length of sting z is {}".format(len_str_z))
@@ -80,6 +131,22 @@ print("the value of sixth index of string z is {}".format(sxth_ind_str_z))
 sevnth_ind_str_z = str_z[7]
 print("the value of seventh index of string z is {}".format(sevnth_ind_str_z))
 
+frst_neg_str_z = str_z[-1]
+print("the value of first negative index of string z is {}".format(frst_neg_str_z))
+scnd_neg_str_z = str_z[-2]
+print("the value of first negative index of string z is {}".format(scnd_neg_str_z))
+thrd_neg_str_z = str_z[-3]
+print("the value of third negative index of string z is {}".format(thrd_neg_str_z))
+frth_neg_str_z = str_z[-4]
+print("the value of fourth negative index of string z is {}".format(frth_neg_str_z))
+ffth_neg_str_z = str_z[-5]
+print("the value of fifth negative index of string z is {}".format(ffth_neg_str_z))
+sxth_neg_str_z = str_z[-6]
+print("the value of sixth negative index of string z is {}".format(sxth_neg_str_z))
+snth_neg_str_z = str_z[-7]
+print("the value of seventh negative index of string z is {}".format(snth_neg_str_z))
+egth_neg_str_z = str_z[-8]
+print("the value of eighth negative index of string z is {}".format(egth_neg_str_z))
 
 
 
