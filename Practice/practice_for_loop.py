caf1dcfa049711c4133ee8fd7e59b5202226e6da
Gajendra <gajendrabg123@gmail.com>
2022-09-30 18:58:46 +0530

    methods

--- Practice/practice_for_loop.py
+++ Practice/practice_for_loop.py
@@ -1,37 +1,30 @@
 """
 i/p :  str_b = "Welcome to python"
 o/p :  rev_str_b = "emocleW ot nothyp"
-
 """
-# str_c = "Welcome to python"
-# rev_str_c = ""
-# for c in str_c:
-#     print("the value of c is {}".format(c))
-#     rev_str_c = c+rev_str_c
-#     print("the value of reverse string is {}".format(rev_str_c))
-#
-# print("the value of reverse string is {}".format(rev_str_c))
-# rev_str_b = rev_str_c[10:17]+rev_str_c[6:10]+rev_str_c[0:7]
-# print("the value of reverse string is {}".format(rev_str_b))
+str_a = "Welcome to python"
+splt_str = str_a.split(" ")
+rev_lst = []
+for i in splt_str:
+    rev_lst.append(i[::-1])
 
-str_b = "Welcome to python"
-splt = str_b.split()
-print(splt)
-rev_str = ""
-for i in splt:
-    rev_str = i[::][::-1]+rev_str
+rev_lst = " ".join(rev_lst)
 
-print(rev_str)
+print(rev_lst)
 
-# rev_splt = splt[0][::-1]+" "+splt[1][::-1]+" "+splt[2][::-1]
-# print(rev_splt)
+list_a = [2, 3, 4, 8]
+sum_list_a = 0
+for i in list_a:
+    sum_list_a = i+sum_list_a
+print(sum_list_a)
 
-# rev_str = " "
-# for r in str_b:
-#     rev_splt = str_b.split()
-#     print(rev_splt)
-#     rev_str = rev_splt[0][::-1]+" "+rev_splt[1][::-1]+" "+rev_splt[2][::-1]
-#     print(rev_str)
+
+cub_lst = []
+for i in list_a:
+    cub_lst.append(i*i*i)
+print(cub_lst)
+
+
 
 
 
--- Practice/practice_methods.py
+++ Practice/practice_methods.py
@@ -10,3 +10,28 @@ print("the value of reversed string is {}".format(str_a))
 
 str_b = str_rev("python")
 print(str_b)
+
+def sum_list(list):
+    sum_lst = 0
+    for i in list:
+        sum_lst = i+sum_lst
+    return sum_lst
+
+
+list_c = sum_list([4, 8, 27, 37])
+print("the sum of the list is {}".format(list_c))
+
+list_d = sum_list([7, 18, 29, 31])
+print("the sum of the list is {}".format(list_d))
+
+def cube_list(list):
+    cub_lst = []
+    for i in list:
+        cub_lst.append(i*i*i)
+    return cub_lst
+
+list_x = cube_list([2, 3, 4, 5])
+print("the cube values are {}".format(list_x))
+
+list_y = cube_list([6, 7, 8, 9, 10])
+print("the cube values are {}".format(list_y))
